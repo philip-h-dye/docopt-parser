3b47958cd34c597d2dc8594ce42b3904b5d42a90
philip-h-dye <philip.h.dye@gmail.com>
2021-04-14 13:19:44 -0400

    added github url

--- setup.py
+++ setup.py
@@ -20,24 +20,22 @@ class PreInstallCommand(InstallCommand):
 slurp = lambda fname : [(f.read(), f.close()) for f in [open(fname,'r')]][0][0]
 
 contents = slurp('docopt.peg')
-
-print("[docopt.peg]\n{contents[:50]}\n\n")
-
 vpattern = ".*#\s*__version__\s*=\s*[\"'](.*)[\"']"
 _version = re.search ( vpattern, contents, re.M ).group(1)
 
 with open('README.rst', 'rb') as f:
     _long_description = f.read().decode('utf-8')
 
+#------------------------------------------------------------------------------
+
 setup(
     name = 'docopt-parser',
     version = _version,
     description = "Exploratory PEG Grammar for docopt style usaged text.",
     author = 'Philip H. Dye',
-    author_email = 'philip@phd-solutions.com',
-    # url = 'http://www.phd-solutions.com/philip-d-dye',
+    url = 'https://github.com/philip-h-dye/docpt-parser',
     long_description = _long_description,
-    # py_modules=['docopt_parser'], # none
+    # py_modules=['docopt_parser'], # none yet
     cmdclass={
         'install': PreInstallCommand,
     },
