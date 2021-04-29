048c7a7f5d9117c3b0a3d3a878fd3fbcfab07bfd
philip-h-dye <philip.h.dye@gmail.com>
2021-04-29 14:52:10 -0400

    just a bit of cleanup, no functional change -- grammar/python/generic/operand.py

--- grammar/python/generic/operand.py
+++ grammar/python/generic/operand.py
@@ -2,11 +2,12 @@ from arpeggio import OrderedChoice, RegExMatch as _
 
 ALL = [ 'operand', 'operand_angled', 'operand_all_caps' ]
 
-def operand_angled():	return _(r'<[-_:\w]+>',
-                                 rule_name='operand_angled')
+def operand_angled():
+    return _(r'<[-_:\w]+>', rule_name='operand_angled')
 
-def operand_all_caps():	return _(r'[A-Z][_A-Z0-9]+\b',
-                                 rule_name='operand_all_caps')
+def operand_all_caps():
+    # '\b' is required to not improperly match 'FOO' of 'FOObar'
+    return _(r'[A-Z][_A-Z0-9]+\b', rule_name='operand_all_caps')
 
 def operand():
     return OrderedChoice( [ operand_angled , operand_all_caps ],
