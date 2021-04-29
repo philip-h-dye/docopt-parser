1ac071f7e5a561651985513acdf6a28abff1bed6
philip-h-dye <philip.h.dye@gmail.com>
2021-04-29 15:47:09 -0400

    Removed outdated grammar/python/{optdesc,generic}-old along with their tests/...

--- grammar/python/generic-old/option/short/000.py
+++
@@ -1,6 +0,0 @@
-def number():     return _(r'\d*\.\d*|\d+')
-def factor():     return Optional(["+","-"]), [number,
-                          ("(", expression, ")")]
-def term():       return factor, ZeroOrMore(["*","/"], factor)
-def expression(): return term, ZeroOrMore(["+", "-"], term)
-def calc():       return OneOrMore(expression), EOF
