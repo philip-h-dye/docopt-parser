15909d4f99f9f19b71453b63845db46c5c5d5016
philip-h-dye <philip.h.dye@gmail.com>
2021-05-02 13:45:33 -0400

    Uppercase names in character table, fix rule generation -- grammar/python/common.py

--- grammar/python/common.py
+++ grammar/python/common.py
@@ -17,36 +17,54 @@ ALL = ( # single character constants and rules are added dynamically
 #------------------------------------------------------------------------------
 
 CHARACTER_NAME_TO_CHAR = {
-    'tab'               : '\t' , #   9  0x09
-    'lf'                : '\n' , #  10  0x0a
-    'cr'                : '\r' , #  13  0x0d
-    'space'             : ' '  , #  32  0x20
-    'l_paren'           : '('  , #  40  0x28
-    'r_paren'           : ')'  , #  41  0x29
-    'comma'             : ','  , #  44  0x2c
-    'eq'                : '='  , #  61  0x32
-    'l_bracket'         : '['  , #  91  0x5b
-    'r_bracket'         : ']'  , #  94  0x5d
-    'bar'               : '|'  , # 124  0x7c
+    'TAB'               : '\t' , #   9  0x09
+    'LF'                : '\n' , #  10  0x0a
+    'CR'                : '\r' , #  13  0x0d
+    'SPACE'             : ' '  , #  32  0x20
+    'L_PAREN'           : '('  , #  40  0x28
+    'R_PAREN'           : ')'  , #  41  0x29
+    'COMMA'             : ','  , #  44  0x2c
+    'EQ'                : '='  , #  61  0x32
+    'L_BRACKET'         : '['  , #  91  0x5b
+    'R_BRACKET'         : ']'  , #  94  0x5d
+    'BAR'               : '|'  , # 124  0x7c
 }
 
 #------------------------------------------------------------------------------
 
-CHARACTER_CHAR_TO_NAME = { }
+def create_character_rule(method_name, ch_name):
+    code = f"""
+def {method_name} ():
+    return {ch_name}
+"""
+    exec(code, globals())
 
-def create_character_rule(ch):
-    def the_method():
-        nonlocal ch
-        return ch
-    return the_method
+    return eval(method_name)
+
+#------------------------------------------------------------------------------
 
 module = sys.modules[__name__]
+
+CHARACTER_CHAR_TO_NAME = { }
+CHARACTER_CHARS = [ ]
+CHARACTER_RULES = [ ]
+
 for name, ch in CHARACTER_NAME_TO_CHAR.items():
+    assert name == name.upper(), \
+        "As constants, character names should be uppercase."
     CHARACTER_CHAR_TO_NAME[ch] = name
-    setattr ( module, name.upper(), ch )
-    setattr ( module, name, create_character_rule(ch) )
+    method_name = name.lower()
+    setattr ( module, name, ch )
+    rule = create_character_rule ( method_name, name )
+    CHARACTER_CHARS.append(ch)
+    CHARACTER_RULES.append(rule)
     ALL.append(name)
-    ALL.append(name.upper)
+    ALL.append(method_name)
+
+del module
+del name
+del ch
+del rule
 
 #------------------------------------------------------------------------------
 
@@ -67,7 +85,7 @@ def ws():
 WX_REGEX = WHITESPACE_REGEX + '*'
 def wx():
     """Zero or more whitespace characters (often '_' in PEG)"""
-    return _(WX_REGEX, rule_name='ws', skipws=False )
+    return _(WX_REGEX, rule_name='wx', skipws=False )
 
 NEWLINE_REGEX = WX_REGEX + '\n'
 def newline():
