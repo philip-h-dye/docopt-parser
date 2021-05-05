b1fd096b6c7e65ca1d193be54ea9c87acf152135
philip-h-dye <philip.h.dye@gmail.com>
2021-05-05 07:35:47 -0400

    Additional tests

--- tests/grammar/python/optdesc/test_section.py
+++ tests/grammar/python/optdesc/test_section.py
@@ -15,7 +15,7 @@ from arpeggio import RegExMatch as _
 #------------------------------------------------------------------------------
 
 from prettyprinter import cpprint as pp, pprint as pp_plain
-from docopt_parser.parsetreenodes import NonTerminal_eq_structural
+from docopt_parser.parsetreenodes import nodes_equal
 from p import pp_str
 
 #------------------------------------------------------------------------------
@@ -23,19 +23,27 @@ from p import pp_str
 from grammar.python.common import ws, newline, COMMA, BAR
 from grammar.python.operand import operand, operand_all_caps, operand_angled
 from grammar.python.option import *
-# # option, ...
 
-# option_list, ol_first_option, ol_term
 from grammar.python.optdesc.list import *
 from grammar.python.optdesc.line import *
 from grammar.python.optdesc.section import *
 
 from docopt_parser import DocOptListViewVisitor
 
-from optsect import tprint, document, body, element, create_expect
-from optsect import create_expect, create_terms
-from optsect import expect_document, expect_ol_line
-from optsect import section_optdesc
+from .optsect import tprint, document, body, element
+from .optsect import create_terms
+from .optsect import expect_document
+from .optsect import section_optdesc
+
+#------------------------------------------------------------------------------
+
+# Refactoring to use *Def object specifications, isolated in *_obj functions :
+from .optsect import section_optdesc_obj
+
+# Shortcuts for creating specification objects
+from .optlist import OptionDef as opt, OptionListDef as olst
+from .optline import OptionLineDef as ol
+from .optsect import OptionDescDef as od
 
 #------------------------------------------------------------------------------
 
@@ -73,14 +81,6 @@ class Test_Usage_Section ( unittest.TestCase ) :
 
     #--------------------------------------------------------------------------
 
-    def _test_show_parse(self):
-        input = ( "  -f  F flag\n"
-                  "  -x  X flag\n" )
-        parsed = self.parser.parse(input)
-        tprint("[parsed]") ; pp(parsed)
-
-    #--------------------------------------------------------------------------
-
     def test_minimal(self):
 
         ol_line_specs = [
@@ -95,67 +95,204 @@ class Test_Usage_Section ( unittest.TestCase ) :
         # with open ("expect.txt", 'w') as f :
         #     pp_plain(expect, stream=f)
 
-        tprint(f"\nOptions :\n{text}")
-
-        parsed = self.parser.parse(text)
-        # tprint("[parsed]") ; pp(parsed)
-        # with open ("parsed.txt", 'w') as f :
-        #     pp_plain(parsed, stream=f)
-
-        assert parsed == expect, ( f"input = '{input}' :\n"
-                                   f"[expect]\n{pp_str(expect)}\n"
-                                   f"[parsed]\n{pp_str(parsed)}" )
+        self.parse_and_verify ( text, expect )
 
     #--------------------------------------------------------------------------
 
-    def WIP_test_minimal_obj(self):
-
-        from optlist import OptionDef as opt, OptionListDef as olst
-        from optline import OptionLineDef as ol
+    def test_minimal_obj__step_by_step (self):
 
         olst_1   = olst ( opt( '-h', ), opt( '--help', ) )
         oline_1  = ol   ( olst_1, "Show this usage information." )
         olst_2   = olst ( opt( '-v', ), opt( '--version', ) )
         oline_2  = ol   ( olst_2, "Print the version and exit." )
-        ol_line_specs = [ oline_1, oline_2 ]
+        optspecs = od   ( oline_1, oline_2, intro="Options :" )
 
-        ( text, opt_desc ) = section_optdesc_obj ( ol_line_specs )
+        # print("[optspecs]") ; pp(optspecs)
+
+        ( text, opt_desc ) = section_optdesc_obj ( optspecs )
+
+        # print(f"[test] text :\n{text}\n")
+        # print(f"[test] opt_desc :\n{pp_str(opt_desc)}\n")
 
         expect = expect_document ( [ opt_desc ] )
         # tprint("[expect]") ; pp(expect)
         # with open ("expect.txt", 'w') as f :
         #     pp_plain(expect, stream=f)
 
-        tprint(f"\nOptions :\n{text}")
-
-        parsed = self.parser.parse(text)
-        # tprint("[parsed]") ; pp(parsed)
-        # with open ("parsed.txt", 'w') as f :
-        #     pp_plain(parsed, stream=f)
-
-        assert parsed == expect, ( f"input = '{input}' :\n"
-                                   f"[expect]\n{pp_str(expect)}\n"
-                                   f"[parsed]\n{pp_str(parsed)}" )
+        self.parse_and_verify ( text, expect )
 
     #--------------------------------------------------------------------------
 
-    def SKIP_test_create_expect(self):
+    def test_minimal_obj__single_spec (self):
 
-        input = '-f -x -l'
+        optspecs = od (
+            ol ( olst ( opt( '-h', ), opt( '--help', ) ) ,
+                 "Show this usage information." ) ,
+            ol ( olst ( opt( '-v', ), opt( '--version', ) ) ,
+                 "Print the version and exit." ) ,
+            intro="Options :" )
 
-        parsed = self.parser.parse(input)
+        # print("[optspecs]") ; pp(optspecs)
+
+        ( text, opt_desc ) = section_optdesc_obj ( optspecs )
+
+        expect = expect_document ( [ opt_desc ] )
+
+        self.parse_and_verify ( text, expect )
+
+    #--------------------------------------------------------------------------
+
+    # Three bugs for the price of one !
+    #
+    #   In the option-list, operands may be either part of an option or a term.
+    #
+    #   opt( '--file', '=', '<file>') :
+    #      <file> is part of the term long_eq_arg of '--file'.
+    #
+    #   opt( '--query', ' ', '<query>') :
+    #      <query> is a term itself, separate from long_no_arg '--query'.
+    #
+    # 1. optline was inserting a comma rather than a space gap before
+    #    operands.
+    # 2. All optline/optlist term except handlers assumed all terms were
+    #    options and wrapped them in an option() Nonterminal.  This wrapping
+    #    neeed to be pushed down into the term__{long,short}_* functions.
+    #
+    # 3. Unexpected space gap between option-list and newline when no help
+    #    provided.  Due to offset, without help, it must be 0.
+    #
+    # Inner node focused analysis is left as an example. Smaller parse trees
+    # are much easier to digest quickly.
+    #
+    def test_space_arg (self):
+
+        optspecs = od (
+            ol ( olst ( opt( '--query', ' ', '<query>' ) ) ) ,
+            # ol ( olst ( opt( '-f', ) ) ) ,
+            # ol ( olst ( opt( '-f', ) , opt( '--file', '=', '<file>' ) ) ,
+            #      "File to load." ) ,
+            # ol ( olst ( opt( '-x', '', 'FILE' ), opt( '--extract', ) ) ,
+            #      "Extract file." ) ,
+            ol ( olst ( opt( '-x', '', 'FILE' ) ) ) ,
+            intro="Options :", offset=18 )
+
+        # print("[optspecs]") ; pp(optspecs)
+
+        ( text, opt_desc ) = section_optdesc_obj ( optspecs )
+
+        expect = expect_document ( [ opt_desc ] )
+
+        parsed = self.parse ( text, expect )
+
+        # improper comma separator for operand bug
+        if False :
+            expect = expect[0][0][0][0][1][1][0]
+            parsed = parsed[0][0][0][0][1][1][0]
+
+        # improper 'option' wrapper for 'operand'
+        if False :
+            expect = expect[0][0][0][0][1][1][1]
+            parsed = parsed[0][0][0][0][1][1][1]
+
+        self.verify ( text, expect, parsed )
+
+    #--------------------------------------------------------------------------
+
+    def test_some_args (self): # '--query <query>', expect includes comma
+
+        optspecs = od (
+            ol ( olst ( opt( '-f', ), opt( '--file', '=', '<file>' ) ) ,
+                 "File to load." ) ,
+            ol ( olst ( opt( '-x', '', 'EXTRACT' ), opt( '--extract', ) ) ,
+                 "Extract file." ) ,
+            ol ( olst ( opt( '-m', ' ', 'MEMBER' ), opt( '--member', ) ) ,
+                 "member ..." ) ,
+            ol ( olst ( opt( '-q', ), opt( '--query', ' ', '<query>' ) ) ,
+                 "Query ..." ) ,
+            ol ( olst ( opt( '-h', ), opt( '--help', ) ) ,
+                 "Show this usage information." ) ,
+            ol ( olst ( opt( '-v', ), opt( '--version', ) ) ,
+                 "Print the version and exit." ) ,
+            intro="Options :", offset=18 )
+
+        # print("[optspecs]") ; pp(optspecs)
+
+        ( text, opt_desc ) = section_optdesc_obj ( optspecs )
+
+        expect = expect_document ( [ opt_desc ] )
+
+        self.parse_and_verify(text, expect)
+
+    #--------------------------------------------------------------------------
+
+    def parse_and_verify ( self, text, expect ) :
+        self.verify ( text, expect, self.parse ( text, expect ) )
+
+    #--------------------------------------------------------------------------
+
+    def parse ( self, text, expect ) :
+
+        # tprint(f"\nOptions :\n{text}")
+
+        # with open ("scratch/expect.txt", 'w') as f :
+        #     pp_plain(expect, stream=f)
+
+        try :
+            parsed = self.parser.parse(text)
+        except :
+            print("\nParse FAILED\n"
+                  f"[expect]\n{pp_str(expect)}\n"
+                  f"text = '{text}' :\n" )
+            assert 1 == 0
+
         # tprint("[parsed]") ; pp(parsed)
 
-        expect = create_expect (
-            Terminal( short_no_arg(), 0, '-f' ) ,
-            Terminal( short_no_arg(), 0, '-x' ) ,
-            Terminal( short_no_arg(), 0, '-l' ) ,
-            eof = ( input[-1] != '\n' ) ,
-        )
+        return parsed
 
-        assert parsed == expect, ( f"input = '{input}' :\n"
-                                   f"[expect]\n{pp_str(expect)}\n"
-                                   f"[parsed]\n{pp_str(parsed)}" )
+    #--------------------------------------------------------------------------
+
+    def verify ( self, text, expect, parsed ) :
+
+        with open ("scratch/expect.txt", 'w') as f :
+                pp_plain(expect, stream=f)
+        with open ("scratch/parsed.txt", 'w') as f :
+            pp_plain(parsed, stream=f)
+
+        if False :
+            nth_option_line = 0
+            expect = expect[0][0] # [0][ nth_option_line ] # [0] [0]
+            parsed = parsed[0][0] # [0][ nth_option_line ] # [0] [0]
+
+            with open ("scratch/expect.txt", 'w') as f :
+                    pp_plain(expect, stream=f)
+            with open ("scratch/parsed.txt", 'w') as f :
+                pp_plain(parsed, stream=f)
+
+            print('')
+            print(f"[expect] rule '{expect.rule_name}' with {len(expect)} children")
+            print(f"[parsed] rule '{parsed.rule_name}' with {len(parsed)} children")
+
+            for i in range(len(expect)) :
+                if not nodes_equal( parsed[i], expect[i]) :
+                    print ( f"text = '{text}' :\n"
+                            f"[expect]\n{pp_str(expect[i])}\n"
+                            f"[parsed]\n{pp_str(parsed[i])}" )
+                    assert 1 == 0
+
+            if len(expect) < len(parsed) :
+                start = len(expect) # - 1
+                print ( f"text = '{text}' :\n"
+                        f"[expect]\n{pp_str(expect[start:])}\n"
+                        f"[parsed]\n{pp_str(parsed[start:])}" )
+                assert 1 == 0
+
+        # print("[expect]");  pp(expect) ; print("[parsed]"); pp(parsed)
+        # return
+
+        assert nodes_equal(parsed, expect), \
+            ( f"text = '{text}' :\n"
+              f"[expect]\n{pp_str(expect)}\n"
+              f"[parsed]\n{pp_str(parsed)}" )
 
     #--------------------------------------------------------------------------
 
