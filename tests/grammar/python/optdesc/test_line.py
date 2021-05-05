d9c533b2e75ffd4993ae3ee28905a8b17a321fe1
philip-h-dye <philip.h.dye@gmail.com>
2021-05-05 07:11:22 -0400

    Updated with option() NonTerminal wrapper for args.  Updated with parse_and_verify.

--- tests/grammar/python/optdesc/test_line.py
+++ tests/grammar/python/optdesc/test_line.py
@@ -14,8 +14,9 @@ from arpeggio import RegExMatch as _
 
 #------------------------------------------------------------------------------
 
-from prettyprinter import cpprint as pp
+from prettyprinter import cpprint as pp, pprint as pp_plain
 from docopt_parser.parsetreenodes import NonTerminal_eq_structural
+from docopt_parser.parsetreenodes import nodes_equal
 from p import pp_str
 
 #------------------------------------------------------------------------------
@@ -23,15 +24,15 @@ from p import pp_str
 from grammar.python.common import ws, newline, COMMA, BAR
 from grammar.python.operand import operand, operand_all_caps, operand_angled
 from grammar.python.option import *
-# # option, ...
 
-# option_list, ol_first_option, ol_term
 from grammar.python.optdesc.list import *
 from grammar.python.optdesc.line import *
 
 from docopt_parser import DocOptListViewVisitor
 
-from optline import tprint, ogenerate, document, body, element, create_expect
+from .optline import tprint, ogenerate, document, body, element, create_expect
+
+from util import write_scratch
 
 #------------------------------------------------------------------------------
 
@@ -63,57 +64,164 @@ class Test_Option_Line ( unittest.TestCase ) :
         tprint._on = self.show or self.debug is not False
 
         # grammar_elements = [ option_list, ws ]
-        self.parser = ParserPython( language_def=document, skipws=False,
+        self.grammar = document
+        self.parser = ParserPython( language_def=self.grammar, skipws=False,
                                     debug = parse_debug, )
-        # # NEVER # reduce_tree=True -- needed meaning is lost
+        
+        # NEVER # reduce_tree=True -- needed meaning is lost
+
+    #--------------------------------------------------------------------------
+
+    def single ( self, rule, text, expect ):
+        # both rule and text get a single space prefix to assist when
+        # the rule has a whitespace lookahead
+        #
+        # The model unfortunately will show 'rule' as a function
+        # rather that it's expression.  It is tempting to then instantiate
+        # it as rule().  The parse model now show it nicely.
+        #
+        # Unfortunately, the resulting parse tree drops the node for rule.
+        #
+        def grammar():
+            return Sequence( ( wx, rule, EOF ) ,
+                             rule_name='grammar', skipws=True )
+        self.verify_grammar ( grammar, ' '+text, expect )
+
+    #--------------------------------------------------------------------------
+
+    def multiple_spaced ( self, rule, text, expect ):
+        # both rule and text get a single space prefix to assist when
+        # the rule has a whitespace lookahead
+        def grammar():
+            return Sequence( ( OrderedChoice ( [ rule(), ws, newline ] ), EOF ),
+                             rule_name='grammar', skipws=True )
+        self.verify_grammar ( grammar, ' '+text, expect )
+
+    #--------------------------------------------------------------------------
+
+    def verify_grammar ( self, grammar, text, expect ):
+        self.grammar = grammar()
+        expect = NonTerminal( self.grammar, [ expect, t_eof ] )
+        self.parser = ParserPython ( grammar, skipws=False, reduce_tree=False )
+        self.parse_and_verify( text, expect )
+
+    #--------------------------------------------------------------------------
+
+    def parse_and_verify ( self, text, expect ) :
+        self.verify ( text, expect, self.parse ( text, expect ) )
+
+    #--------------------------------------------------------------------------
+
+    def parse ( self, text, expect ) :
+
+        # tprint(f"\nOptions :\n{text}")
+
+        write_scratch( call={'fcn' : 'parse' },
+                       grammar=self.grammar, text=text,
+                       expect=expect, expect_f=flatten(expect),
+                       model=self.parser.parser_model, _clean=True, )
+        try :
+            # print(f"\n: text = '{text}'")
+            parsed = self.parser.parse(text)
+            write_scratch( parsed=parsed )
+        except Exception as e :
+            print("\n"
+                  f"[expect]\n{pp_str(expect)}\n\n"
+                  f"text = '{text}' :\n\n"
+                  f"Parse FAILED :\n"
+                  f"{str(e)}" )
+            raise
+
+        # tprint("[parsed]") ; pp(parsed)
+
+        with open ("scratch/parsed.txt", 'w') as f :
+            pp_plain(expect, stream=f)
+
+        return parsed
+
+    #--------------------------------------------------------------------------
+
+    def verify ( self, text, expect, parsed ) :
+
+        with open ("scratch/expect.txt", 'w') as f :
+                pp_plain(expect, stream=f)
+        with open ("scratch/parsed.txt", 'w') as f :
+            pp_plain(parsed, stream=f)
+
+        if False :
+            nth_option_line = 0
+            expect = expect[0][0] # [0][ nth_option_line ] # [0] [0]
+            parsed = parsed[0][0] # [0][ nth_option_line ] # [0] [0]
+
+            with open ("scratch/expect.txt", 'w') as f :
+                    pp_plain(expect, stream=f)
+            with open ("scratch/parsed.txt", 'w') as f :
+                pp_plain(parsed, stream=f)
+
+            print('')
+            print(f"[expect] rule '{expect.rule_name}' with {len(expect)} children")
+            print(f"[parsed] rule '{parsed.rule_name}' with {len(parsed)} children")
+
+            for i in range(len(expect)) :
+                if not nodes_equal( parsed[i], expect[i]) :
+                    print ( f"text = '{text}' :\n"
+                            f"[expect]\n{pp_str(expect[i])}\n"
+                            f"[parsed]\n{pp_str(parsed[i])}" )
+                    assert 1 == 0
+
+            if len(expect) < len(parsed) :
+                start = len(expect) # - 1
+                print ( f"text = '{text}' :\n"
+                        f"[expect]\n{pp_str(expect[start:])}\n"
+                        f"[parsed]\n{pp_str(parsed[start:])}" )
+                assert 1 == 0
+
+        # print("[expect]");  pp(expect) ; print("[parsed]"); pp(parsed)
+
+        if False :
+            # Less flashly output sometime useful when debugging
+            if not nodes_equal(parsed, expect) :
+                print("\n!!!")
+                print("!!! no match")
+                print("!!!")
+            return
+
+        assert nodes_equal(parsed, expect), \
+            ( f"text = '{text}' :\n"
+              f"[expect]\n{pp_str(expect)}\n"
+              f"[parsed]\n{pp_str(parsed)}" )
 
     #--------------------------------------------------------------------------
 
     def test_single_short_no_arg (self):
         input = '-f'
-        parsed = self.parser.parse(input)
-        # tprint("[parsed]") ; pp(parsed)
-
         expect = create_expect (
-            Terminal( short_no_arg(), 0, '-f' ) ,
+            NonTerminal( option(), [ Terminal( short_no_arg(), 0, '-f' ) ] ) ,
             eof = ( input[-1] != '\n' ) ,
         )
-
-        assert NonTerminal_eq_structural(parsed, expect), \
-            ( f"input = '{input}' :\n"
-              f"[expect]\n{pp_str(expect)}\n"
-              f"[parsed]\n{pp_str(parsed)}" )
+        self.parse_and_verify( input, expect )
 
     #--------------------------------------------------------------------------
 
     def test_single_short_w_arg (self):
         input = '-fNORM'
-        parsed = self.parser.parse(input)
-        # tprint("[parsed]") ; pp(parsed)
-
         expect = create_expect (
-            NonTerminal( short_adj_arg(), [
-                Terminal( short_adj_arg__option(), 0, '-f' ) ,
-                NonTerminal( operand(), [
-                    Terminal( operand_all_caps(), 0, 'NORM' ) ,
+            NonTerminal( option(), [ 
+                NonTerminal( short_adj_arg(), [
+                    Terminal( short_adj_arg__option(), 0, '-f' ) ,
+                    NonTerminal( operand(), [
+                        Terminal( operand_all_caps(), 0, 'NORM' ) ,
+                    ]) ,
                 ]) ,
             ]) ,
             eof = ( input[-1] != '\n' ) ,
         )
-
-        assert NonTerminal_eq_structural(parsed, expect), \
-            ( f"input = '{input}' :\n"
-              f"[expect]\n{pp_str(expect)}\n"
-              f"[parsed]\n{pp_str(parsed)}" )
+        self.parse_and_verify( input, expect )
 
     #--------------------------------------------------------------------------
 
     def test_single (self):
         input = '-f'
-        parsed = self.parser.parse(input)
-        # tprint("[parsed]") ; tprint("\n", parsed.tree_str(), "\n")
-        # tprint("[parsed]") ; pp(parsed)
-
         expect = NonTerminal( document(), [
             NonTerminal( body(), [
                 NonTerminal( element(), [
@@ -131,20 +239,12 @@ class Test_Option_Line ( unittest.TestCase ) :
             ]) ,
             Terminal(EOF(), 0, '') ,
         ])
-
-        assert NonTerminal_eq_structural(parsed, expect), \
-            ( f"input = '{input}' :\n"
-              f"[expect]\n{pp_str(expect)}\n"
-              f"[parsed]\n{pp_str(parsed)}" )
+        self.parse_and_verify( input, expect )
 
     #--------------------------------------------------------------------------
 
     def test_pair (self):
         input = '-f -x'
-        parsed = self.parser.parse(input)
-        # tprint("[parsed]") ; tprint("\n", parsed.tree_str(), "\n")
-        # tprint("[parsed]") ; pp(parsed)
-
         expect = NonTerminal( document(), [
             NonTerminal( body(), [
                 NonTerminal( element(), [
@@ -174,19 +274,13 @@ class Test_Option_Line ( unittest.TestCase ) :
             ]) ,
             Terminal(EOF(), 0, '') ,
         ])
-
-        assert NonTerminal_eq_structural(parsed, expect), \
-            ( f"input = '{input}' :\n"
-              f"[expect]\n{pp_str(expect)}\n"
-              f"[parsed]\n{pp_str(parsed)}" )
+        self.parse_and_verify( input, expect )
 
     #--------------------------------------------------------------------------
 
     def test_trio (self):
+
         input = '-f -x -l'
-        parsed = self.parser.parse(input)
-        # tprint("[parsed]") ; tprint("\n", parsed.tree_str(), "\n")
-        # tprint("[parsed]") ; pp(parsed)
 
         expect = NonTerminal( document(), [
             NonTerminal( body(), [
@@ -230,10 +324,7 @@ class Test_Option_Line ( unittest.TestCase ) :
             Terminal(EOF(), 0, '') ,
         ])
 
-        assert NonTerminal_eq_structural(parsed, expect), \
-            ( f"input = '{input}' :\n"
-              f"[expect]\n{pp_str(expect)}\n"
-              f"[parsed]\n{pp_str(parsed)}" )
+        self.parse_and_verify( input, expect )
 
     #--------------------------------------------------------------------------
 
@@ -242,25 +333,22 @@ class Test_Option_Line ( unittest.TestCase ) :
         input = '-f -x -l'
 
         expect = create_expect (
-            Terminal( short_no_arg(), 0, '-f' ) ,
-            Terminal( short_no_arg(), 0, '-x' ) ,
-            Terminal( short_no_arg(), 0, '-l' ) ,
+            NonTerminal( option(), [ Terminal( short_no_arg(), 0, '-f' ) ] ) ,
+            NonTerminal( option(), [ Terminal( short_no_arg(), 0, '-x' ) ] ) ,
+            NonTerminal( option(), [ Terminal( short_no_arg(), 0, '-l' ) ] ) ,
             eof = ( input[-1] != '\n' ) ,
         )
 
         # print("[ expect ]")
         # pp(expect[0][0][0][0][1])
 
-        parsed = self.parser.parse(input)
+        parsed = self.parse(input, expect)
         # tprint("[parsed]") ; pp(parsed)
 
         # print("[ parsed ]")
         # pp(parsed[0][0][0][0][1])
 
-        assert NonTerminal_eq_structural(parsed, expect), \
-            ( f"input = '{input}' :\n"
-              f"[expect]\n{pp_str(expect)}\n"
-              f"[parsed]\n{pp_str(parsed)}" )
+        self.verify( input, expect, parsed )
 
     #--------------------------------------------------------------------------
 
@@ -272,9 +360,9 @@ class Test_Option_Line ( unittest.TestCase ) :
 
 #------------------------------------------------------------------------------
 
-def lgenerate ( cls, optdefs, help_, indent='  ', sep=', ', offset=16 ):
+def generate_line ( cls, optdefs, help_, indent='  ', sep=', ', offset=16 ):
 
-    # tprint(f"lgenerate :  optdefs = {optdefs}, help_ = '{help_}, sep='{sep}' )")
+    # tprint(f"generate_line :  optdefs = {optdefs}, help_ = '{help_}, sep='{sep}' )")
 
     ( method_name, optlist_string ) = ogenerate ( cls, optdefs, sep=sep, )
 
@@ -301,38 +389,40 @@ if False :
 
     text = ''
     for ol_spec in olines :
-        text += lgenerate ( Test_Option_Line, *ol_spec )
+        text += generate_line ( Test_Option_Line, *ol_spec )
 
     print(f"Options:\n{text}")
 
 #------------------------------------------------------------------------------
 
-def tgenerate ( optdef, *args, **kwargs ):
-    # OLD # ogenerate ( optdef, *args, cls=Test_Option_Line, **kwargs )
-    # ogenerate ( Test_Option_Line, optdef, *args, **kwargs )
-    pass
+def _generate ( optdefs, *args, **kwargs ):
+    return ogenerate ( Test_Option_Line, optdefs, *args, **kwargs )
 
 #------------------------------------------------------------------------------
 
 # boundry condition, the first option is handled separately from succeeding terms
 # and it is an ol_first_option, not an ol_term
 # generate( '-f' )
-tgenerate ( ( ( '-f', ), ) )
+_generate ( ( ( '-f', ), ) )
 
 #------------------------------------------------------------------------------
 
-if False  :
+if False :
+    pass
+
+if True :
+    pass
 
     # boundry condition, '-x' is first ol_term of the option_list's ZeroToMany and
     # the first possible position for a option-argument
     # generate( '-f -x' )
-    tgenerate ( ( ( '-f', ) ,
+    _generate ( ( ( '-f', ) ,
                   ( '-x', ) ,
                 ) )
 
     # one past boundry condition, first term on on a boundry
     # generate('-f -x -l')
-    tgenerate ( ( ( '-f', ) ,
+    _generate ( ( ( '-f', ) ,
                   ( '-x', ) ,
                   ( '-l', ) ,
                 ) )
@@ -341,12 +431,12 @@ if False  :
     # generate("--file --example")
     # generate("--file --example --list")
 
-    tgenerate ( ( ( '--file', ) ,
+    _generate ( ( ( '--file', ) ,
                 ) )
-    tgenerate ( ( ( '--file', ) ,
+    _generate ( ( ( '--file', ) ,
                   ( '--example', ) ,
                 ) )
-    tgenerate ( ( ( '--file', ) ,
+    _generate ( ( ( '--file', ) ,
                   ( '--example', ) ,
                   ( '--list', ) ,
                 ) )
@@ -355,20 +445,20 @@ if False  :
     # generate("--file=<file> --example=<example>")
     # generate("--file=<file> --example=<example> --list=<list>")
 
-    tgenerate ( ( ( '--file', '=', '<file>', ) ,
+    _generate ( ( ( '--file', '=', '<file>', ) ,
                 ) )
 
-    tgenerate ( ( ( '--file', '=', '<file>', ) ,
+    _generate ( ( ( '--file', '=', '<file>', ) ,
                   ( '--example', '=', '<example>', ) ,
                 ) )
 
-    tgenerate ( ( ( '--file', '=', '<file>', ) ,
+    _generate ( ( ( '--file', '=', '<file>', ) ,
                   ( '--example', '=', '<example>', ) ,
                   ( '--list', '=', '<list>', ) ,
                 ) )
 
     # generate("--file=<FILE> -x --example=<EXAMPLE> -y --query=<QUERY> -q")
-    tgenerate ( ( ( '--file', '=', '<FILE>', ) ,
+    _generate ( ( ( '--file', '=', '<FILE>', ) ,
                   ( '-x', ) ,
                   ( '--example', '=', '<EXAMPLE>', ) ,
                   ( '-y', ) ,
@@ -377,31 +467,32 @@ if False  :
                 ) )
 
     # generate("--file=FILE -x")
-    tgenerate ( ( ( '--file', '=', 'FILE', ) ,
+    _generate ( ( ( '--file', '=', 'FILE', ) ,
                   ( '-x', ) ,
                 ) )
 
-    # generate("--file=FOObar -x")
+    # generate("--file=FOObar -x")    
     if False  :
-        tgenerate ( ( ( '--file', '=', 'FOObar', ) ,
+        # FIXME: not implemened yet -- 'command/example' option-argument
+        _generate ( ( ( '--file', '=', 'FOObar', ) ,
                       ( '-x', ) ,
                     ) )
 
     # generate("--file=a|b|c -x")
     if False  :
-        # FIXME: not implemented yet
-        tgenerate ( ( ( '--file', '=', 'a|b|c', ) ,
+        # FIXME: not implemened yet -- 'command/example' option-argument
+        _generate ( ( ( '--file', '=', 'a|b|c', ) ,
                       ( '-x', ) ,
                     ) )
 
     #------------------------------------------------------------------------------
 
-    tgenerate ( ( ( '--file', '=', 'NORM' ) ,
+    _generate ( ( ( '--file', '=', 'NORM' ) ,
                   ( '--file', ' ', 'NORM' ) ,
                   ( '--file', ) ,
                 ) )
 
-    tgenerate ( ( ( '-f', '', 'NORM' ) ,
+    _generate ( ( ( '-f', '', 'NORM' ) ,
                   ( '-f', ' ', 'NORM' ) ,
                   ( '-f', ) ,
                 ) )
