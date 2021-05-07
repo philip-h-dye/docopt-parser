8642f3f7177e079127420a05dc0c1267f4afdd00
philip-h-dye <philip.h.dye@gmail.com>
2021-05-07 10:59:39 -0400

    Added validate_name() and CHARACTER_CHAR_TO_ALIAS, alias->ch mapping in CHARACTER_NAME_TO_CH

--- grammar/python/common.py
+++ grammar/python/common.py
@@ -1,5 +1,7 @@
 import sys
 
+from keyword import iskeyword
+
 from dataclasses import dataclass
 
 from arpeggio import EOF, RegExMatch as _ , StrMatch, Terminal
@@ -18,15 +20,18 @@ ALL = ( # single character constants and rules are added dynamically
 
 #------------------------------------------------------------------------------
 
+# https://donsnotes.com/tech/charsets/ascii.html
+
 @dataclass
 class character (object):
-    name        : str
-    raw         : str
+    name        : str           # Must be n all uppercase valid identifier
+    raw         : str           # i.e. r'\n' is two characters
     alias       : str           = None
     ch          : str           = None
     name_lc     : str           = None
     rule        : object        = None
     rule_m      : object        = None
+
 
 _c = character
 
@@ -63,41 +68,77 @@ def create_character_rules_and_terminals ( c ):
 
 	t_{c.name_lc} = Terminal({c.name_lc}_m(), 0, {c.name})
 	CHARACTER_TERMINALS.append(t_{c.name_lc})
-	ALL.append(t_{c.name_lc})    
+	ALL.append(t_{c.name_lc})
 """
     exec(code.replace('\t',''), globals())
 
     if c.alias and len(c.alias) > 0:
         c.alias_lc = c.alias.lower()
-        exec(f"t_{c.alias_lc} = t_{c.name_lc} \n"
-             f"ALL.append(t_{c.alias_lc}) \n")
+        exec(f"{c.alias_lc} = {c.name_lc} \n"
+             f"ALL.append({c.alias_lc}) \n"
+             f"{c.alias_lc}_m = {c.name_lc}_m \n"
+             f"ALL.append({c.alias_lc}_m) \n"
+             f"t_{c.alias_lc} = t_{c.name_lc} \n"
+             f"ALL.append(t_{c.alias_lc}) \n" )
+
+#------------------------------------------------------------------------------
+
+def validate_name(which, name, raw):
+
+    p = "PACKAGE CONFIGURATION ERROR, "
+
+    assert not iskeyword(name), \
+        ( f"{p}Invalid character {which} '{name}' for '{raw}', {which} "
+          f"may not be a Python keyword." )
+
+    assert name.isidentifier(), \
+        ( f"{p}Invalid character {which} '{name}' for '{raw}', {which} "
+          f"is not a valid Python identifier." )
+
+    assert name == name.upper(), \
+        ( f"{p}Character {which} '{name}' is not all uppercase.  Character "
+          f"name and alias constants must be all uppercase.  Lowercase "
+          f"reserved for their corresponding grammar rules.")
+
+    assert name not in CHARACTER_NAME_TO_CHAR, \
+        ( f"{p}Character {which} '{name}' for '{raw}', already exists.  Prior "
+          f"occurance for '{CHARACTER_NAME_TO_CHAR[name]}'")
 
 #------------------------------------------------------------------------------
 
 module = sys.modules[__name__]
 
 CHARACTER_CHAR_TO_NAME = { }
+CHARACTER_CHAR_TO_ALIAS = { }
 CHARACTER_NAME_TO_CHAR = { }
+CHARACTER_CHAR_TO_RAW = { }
+CHARACTER_RAW_TO_CHAR = { }
+
+# Arrays in order of occurance in CHARACTER_TABLE, no extra elements for aliases.
 CHARACTER_CHARS = [ ]
+CHARACTER_RAW = [ ]
 CHARACTER_RULES = [ ]
 CHARACTER_MRULES = [ ]
 CHARACTER_TERMINALS = [ ]
 
 for c in CHARACTER_TABLE :
-    assert c.name == c.name.upper(), \
-        ( f"Character name '{c.name}' is not all uppercase.  As constants, "
-          f"character names and aliases must be all uppercase." )
-    c.name_lc = c.name.lower()
+    validate_name('name', c.name, c.raw)
     c.ch = eval(f"'{c.raw}'")
     CHARACTER_CHARS.append(c.ch)
+    CHARACTER_RAW.append(c.raw)
     CHARACTER_CHAR_TO_NAME[c.ch] = c.name
     CHARACTER_NAME_TO_CHAR[c.name] = c.ch
+    CHARACTER_RAW_TO_CHAR[c.raw] = c.ch
+    CHARACTER_CHAR_TO_RAW[c.ch] = c.raw
     setattr ( module, c.name, c.ch )
     ALL.append(c.name)
     if c.alias is not None :
+        validate_name('alias', c.alias, c.raw)
         assert c.alias == c.alias.upper(), \
             ( f"Character alias '{name}' is not all uppercase.  As constants, "
               f"character names and aliases must be all uppercase." )
+        CHARACTER_NAME_TO_CHAR[c.alias] = c.ch
+        CHARACTER_CHAR_TO_ALIAS[c.ch] = c.alias
         setattr ( module, c.alias, c.ch )
         ALL.append(c.alias)
     create_character_rules_and_terminals ( c )
@@ -108,11 +149,13 @@ del module
 
 # WHITESPACE_CHARS = ( TAB , CR , SPACE )
 WHITESPACE_CHARS = TAB + CR + SPACE
+WHITESPACE_RAW   = tuple( [ CHARACTER_CHAR_TO_RAW[ch] for ch in WHITESPACE_CHARS ] )
+WHITESPACE_RAW_STR = ''.join(WHITESPACE_RAW)
 WHITESPACE_RULES = ( tab , cr , space )
 WHITESPACE_NAMES = { ch : CHARACTER_CHAR_TO_NAME[ch] for ch in WHITESPACE_CHARS }
 
-# WHITESPACE_REGEX = '[' + ''.join(WHITESPACE_CHARS) + ']'
-WHITESPACE_REGEX = r'[\t\r ]'
+# WHITESPACE_REGEX = r'[\t\r ]'
+WHITESPACE_REGEX = f"[{WHITESPACE_RAW_STR}]"
 def whitespace():
     """One whitespace character of tab(9), carriage return (10), space (32)"""
     return _(WHITESPACE_REGEX, rule_name='whitespace', skipws=False )
@@ -144,7 +187,7 @@ t_eof = Terminal(EOF(), 0, '')
 
 #------------------------------------------------------------------------------
 
-def t_ws_newline ( text = None ):
+def t_wx_newline ( text = None ):
     """Return an arpeggio.Terminal for newline with the specified whitespace.
        If leading whitespace portion of <text> is not empty, create a newline
        Terminal with the leading whitespace followed by a linefeed.
